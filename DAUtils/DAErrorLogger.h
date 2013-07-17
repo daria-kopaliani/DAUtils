0a721fe09f663cec39e617a86841b26335d62859
Daria Kopaliani <daria.kopaliani@gmail.com>
2013-07-17 18:14:04 +0300

    DAErrorLogger added

---
+++ DAUtils/DAErrorLogger.h
@@ -0,0 +1,20 @@
+//
+//  DAErrorLogger.h
+//  Beat
+//
+//  Created by Daria Kopaliani on 1/30/13.
+//  Copyright (c) 2013 nLink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DAErrorLogger : NSObject
+
++ (void)logError:(NSError *)error;
++ (void)logCoreDataError:(NSError *)error;
++ (void)logFileError:(NSError *)error;
++ (void)logNetworkError:(NSError *)error;
++ (void)logErrorMessage:(NSString *)errorMessage;
++ (void)logSavingContextErrorMessage;
+
+@end
---
+++ DAUtils/DAErrorLogger.m
@@ -0,0 +1,84 @@
+//
+//  DAErrorLogger.m
+//  Beat
+//
+//  Created by Daria Kopaliani on 1/30/13.
+//  Copyright (c) 2013 nLink. All rights reserved.
+//
+
+#import "DAErrorLogger.h"
+
+BOOL const BELoggingErrors = 1;
+BOOL const BELoggingCoreDataErrors = 1;
+BOOL const BELoggingFileErrors = 1;
+BOOL const BELoggingNetworkErrors = 1;
+BOOL const BELoggingErrorMessages = 1;
+BOOL const BELoggingParseErrors = 1;
+
+@interface DAErrorLogger ()
+
++ (void)logDetailedErrorDescription:(NSError *)error;
+
+@end
+
+@implementation DAErrorLogger
+
++ (void)logError:(NSError *)error
+{
+    if (BELoggingErrors && error) {
+        [self logDetailedErrorDescription:error];
+    }
+}
+
++ (void)logCoreDataError:(NSError *)error
+{
+    if (BELoggingCoreDataErrors && error) {
+        [self logDetailedErrorDescription:error];
+    }
+}
+
++ (void)logFileError:(NSError *)error
+{
+    if (BELoggingFileErrors && error) {
+        [self logDetailedErrorDescription:error];
+    }
+}
+
++ (void)logNetworkError:(NSError *)error
+{
+    if (BELoggingNetworkErrors && error) {
+        [self logDetailedErrorDescription:error];
+    }
+}
+
++ (void)logErrorMessage:(NSString *)errorMessage
+{
+    if (BELoggingErrorMessages) {
+        NSLog(@"%@", errorMessage);
+    }
+}
+
++ (void)logSavingContextErrorMessage
+{
+    if (BELoggingErrorMessages) {
+        NSLog(@"could not save context (mainThread - %d)", [[NSThread currentThread] isMainThread]);
+    }
+}
+
+#pragma mark - Private
+
++ (void)logDetailedErrorDescription:(NSError *)error
+{
+    NSLog(@"%@", [error localizedDescription]);
+    NSArray *detailedErrors = [[error userInfo] objectForKey:NSDetailedErrorsKey];
+    if (detailedErrors != nil && [detailedErrors count] > 0) {
+        for (NSError *detailedError in detailedErrors) {
+            NSLog(@"  DetailedError: %@", [detailedError userInfo]);
+        }
+    }
+    else {
+        NSLog(@"  %@", [error userInfo]);
+    }
+}
+
+@end
